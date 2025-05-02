a0b5ee69f8c1e2bf73a5792016193c20030bc3cb
RodrigoMisrahi <romisrahi@gmail.com>
2025-05-02 19:32:55 -0300

    ex3

--- funcoes.py
+++ funcoes.py
@@ -1,13 +1,13 @@
 import random
 
-# def rolar_dados(qnt):
-#     lista = []
-#     i = 0
-#     while i < qnt:
-#         x = random.randint(1, 6)
-#         lista.append(x)
-#         i += 1
-#     return lista
+def rolar_dados(qnt):
+    lista = []
+    i = 0
+    while i < qnt:
+        x = random.randint(1, 6)
+        lista.append(x)
+        i += 1
+    return lista
 
 def guardar_dado(rolados, guardados, indice):
     listar = []
@@ -18,6 +18,20 @@ def guardar_dado(rolados, guardados, indice):
         listag.append(guardados[i])
     listag.append(rolados[indice])
     del listar[indice]
+    lista = []
+    lista.append(listar)
+    lista.append(listag)
+    return lista
+
+def remover_dado(rolados, guardados, indice):
+    listar = []
+    listag = []
+    for i in range(len(rolados)):
+        listar.append(rolados[i])
+    for i in range(len(guardados)):
+        listag.append(guardados[i])
+    listar.append(guardados[indice])
+    del listag[indice]
     lista = []
     lista.append(listar)
     lista.append(listag)
