09cb37007936b3fb7939f0564b35e225e1c197e4
RodrigoMisrahi <romisrahi@gmail.com>
2025-05-02 19:20:37 -0300

    ex2 certo

--- funcoes.py
+++ funcoes.py
@@ -1,10 +1,24 @@
 import random
 
-def rolar_dados(qnt):
+# def rolar_dados(qnt):
+#     lista = []
+#     i = 0
+#     while i < qnt:
+#         x = random.randint(1, 6)
+#         lista.append(x)
+#         i += 1
+#     return lista
+
+def guardar_dado(rolados, guardados, indice):
+    listar = []
+    listag = []
+    for i in range(len(rolados)):
+        listar.append(rolados[i])
+    for i in range(len(guardados)):
+        listag.append(guardados[i])
+    listag.append(rolados[indice])
+    del listar[indice]
     lista = []
-    i = 0
-    while i < qnt:
-        x = random.randint(1, 6)
-        lista.append(x)
-        i += 1
+    lista.append(listar)
+    lista.append(listag)
     return lista
