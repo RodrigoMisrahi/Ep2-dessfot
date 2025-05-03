a1aca36f6ef4ee9bdde7950f80c8d1a6ef10fda6
RodrigoMisrahi <romisrahi@gmail.com>
2025-05-02 22:36:21 -0300

    ex3

--- funcoes.py
+++ funcoes.py
@@ -36,3 +36,12 @@ def remover_dado(rolados, guardados, indice):
     lista.append(listar)
     lista.append(listag)
     return lista
+
+def calcula_pontos_regra_simples(faces):
+    dicio = {}
+    for i in range(6):
+        dicio[i] = 0
+    for i in range(len(faces)):
+        dicio[i] += i
+    return dicio
+print(calcula_pontos_regra_simples([2, 3, 4, 5, 2]))
