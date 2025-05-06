6c1e5c32a2f6a7656a57b2f0d5de9e2fd9cfd888
RodrigoMisrahi <romisrahi@gmail.com>
2025-05-06 19:11:02 -0300

    ex13

--- funcoes.py
+++ funcoes.py
@@ -139,3 +139,21 @@ def faz_jogada(faces, cat, cartela):
     else:
         cartela["regra_avancada"][cat] = avancada[cat]
     return cartela
+
+def imprime_cartela(cartela):
+    print("Cartela de Pontos:")
+    print("-"*25)    
+    for i in range(1, 7):
+        filler = " " * (15 - len(str(i)))
+        if cartela['regra_simples'][i] != -1:
+            print(f"| {i}: {filler}| {cartela['regra_simples'][i]:02} |")
+        else:
+            print(f"| {i}: {filler}|    |")
+    for i in cartela['regra_avancada'].keys():
+        filler = " " * (15 - len(str(i)))
+        if cartela['regra_avancada'][i] != -1:
+            print(f"| {i}: {filler}| {cartela['regra_avancada'][i]:02} |")
+        else:
+            print(f"| {i}: {filler}|    |")
+    print("-"*25)
+
